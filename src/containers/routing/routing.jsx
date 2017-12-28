8e45d3cf8d29e185fe64343265d6e07ad70c4801
mahaboobbasha shaik <mahaboobbasha638@gmail.com>
2017-12-28 17:17:12 +0530

    Modified  routing template

--- src/containers/routing/routing.jsx
+++ src/containers/routing/routing.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import Header from '../header/header';
@@ -9,42 +9,45 @@ import Topics from '../test/topics';
 
 const baseRoute = "/reactt/build";
 
-const Routing = function() {
-    
-    const Topics1 = ({ match }) => (
-        <div>
-            <Router histr>
-            <div>
-                <ul>
-                    <li><Link to={`${match.url}/header`}>Header</Link></li>
-                    <li><Link to={`${match.url}/about`}>Home</Link></li>
-                    <li><Link to={`${match.url}/topics`}>Topics</Link></li>
-                </ul>
-                <Route path={`${match.url}/header`} component={Header} />
-                <Route path={`${match.url}/about`} component={Home} />
-                <Route path={`${match.url}/topics`} component={Topics} />
-                <Route exact path={`${match.url}`} render={() => (
-                <h3>Please select a topic.</h3>
-                )}/>
-            </div>
-            </Router>
-        </div>
-    );
+class Routing extends Component{
 
-    return (
-        <Router>
+    render(){
+        
+        const Topics1 = ({ match }) => (
             <div>
-            <ul>
-                <li><Link to={`${baseRoute}/`}>Home</Link></li>
-                <li><Link to={`${baseRoute}/about`}>About</Link></li>
-                <li><Link to={`${baseRoute}/topics`}>Topics</Link></li>
-            </ul>
-            <Route exact path={`${baseRoute}/`} component={Home} />
-            <Route path={`${baseRoute}/about`} component={About} />
-            <Route path={`${baseRoute}/topics`} component={Topics1} />
+                <Router>
+                <div>
+                    <ul>
+                        <li><Link to={`${match.url}/header`}>Header</Link></li>
+                        <li><Link to={`${match.url}/about`}>Home</Link></li>
+                        <li><Link to={`${match.url}/topics`}>Topics</Link></li>
+                    </ul>
+                    <Route path={`${match.url}/header`} component={Header} />
+                    <Route path={`${match.url}/about`} component={Home} />
+                    <Route path={`${match.url}/topics`} component={Topics} />
+                    <Route exact path={`${match.url}`} render={() => (
+                    <h3>Please select a topic.</h3>
+                    )}/>
+                </div>
+                </Router>
             </div>
-        </Router>
-    );
+        );
+
+        return (
+            <Router>
+                <div>
+                <ul>
+                    <li><Link to={`${baseRoute}/`} >Home</Link></li>
+                    <li><Link to={`${baseRoute}/about`} >About</Link></li>
+                    <li><Link to={`${baseRoute}/topics`} >Topics</Link></li>
+                </ul>
+                <Route exact path={`${baseRoute}/`} component={Home} />
+                <Route path={`${baseRoute}/about`} component={About} />
+                <Route path={`${baseRoute}/topics`} component={Topics1} />
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default Routing;
